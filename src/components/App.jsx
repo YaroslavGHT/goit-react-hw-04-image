655aad4cfc614c7c5135a0ae208a8a7e3351489f
YaroslavGHT <gtx6600@gmail.com>
2024-01-06 22:42:10 +0200

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,39 +10,34 @@ export const  App = () => {
   const [pictures, setPictures] = useState([]);
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState('');
-  const [amountPic, setAmountPic] = useState(0);
+  // const [amountPic, setAmountPic] = useState(0);
   const [loading, setLoading] = useState(false);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [modalImg, setModalImg] = useState('');
   const [morePictures, setMorePictures] = useState(false);
 
-   const fetchPostsByQuery = async (page, search) => {
+  const fetchPostsByQuery = async (page, search) => {
     try {
       setLoading(true);
       const pic = await requesImageSearch(page, search);
       const { total, hits } = pic;
       setPictures(prevPictures => [...prevPictures, ...hits]);
-      setAmountPic(total);
+      setMorePictures(page*12 <= total);
       setLoading(false);
     } catch (error) {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    if (search.match(/[a-zA-Zа-яА-Я0-9]/)) {
+    if (search) {
       fetchPostsByQuery(page, search);
     }
   }, [search, page]);
 
-  useEffect(() => {
-    const check = amountPic >= page * 12;
-    setMorePictures(check);
-  }, [pictures, amountPic, page]);
-  
   const handleSubmit = e => {
     e.preventDefault();
-    const searchValue = e.currentTarget.elements.searchInput.value;
+    const searchValue = e.currentTarget.elements.searchInput.value.trim();
     setSearch(searchValue);
     setPictures([]);
     setPage(1);
